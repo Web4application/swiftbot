e8600ec559a5acf97300a89a4376fe166a372b2d
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-21 18:33:03 -0700

    Update instrumentation.js

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/nextjs";
+export const onRequestError = Sentry.captureRequestError;
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
