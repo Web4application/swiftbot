8e25912d6547103b533387de10be28c99ccf048b
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 20:19:40 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/nextjs";
+import type { Metadata } from "next";
+export function generateMetadata(): Metadata {
+  return {
+    // ... your existing metadata
+    other: {
+      ...Sentry.getTraceData(),
+    },
+  };
+}
