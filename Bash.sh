e03b9b308ea6aa8eeb428f4f0d48cf366b2eca77
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 20:33:13 +0200

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -4,3 +4,5 @@ git push origin main
 
 npx @sentry/wizard@latest -i nextjs --saas --org web4app --project swiftbot
 npm install @sentry/nextjs --save
+
+npx create-liveblocks-app@latest --init --framework react
