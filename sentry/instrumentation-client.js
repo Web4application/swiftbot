284d45275d33826d34f83ad10d9574d602beb47b
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 20:27:12 +0200

    Update and rename instrumentation-client.Js to sentry/instrumentation-client.js

--- sentry/instrumentation-client.js
+++ sentry/instrumentation-client.js
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 Sentry.init({
-  dsn: "https://55038c1e414932ec8d81c34e5ef120fa@o4509364065402880.ingest.de.sentry.io/4509364112457808",
+  dsn: "https://o4509364065402880.ingest.de.sentry.io/4509364112457808",
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
