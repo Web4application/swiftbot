c4d23fd821040b3cdf6e96a01c2421659a326539
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-04 01:32:21 +0200

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash.sh
+++ bash.sh
@@ -2,4 +2,9 @@
 docker build -t swiftbot .
 
 # Run the Docker container
-docker run -p 3000:3000 swiftbot
+docker run -p 8000:8000 swiftbot
+
+git branch -m codespace-bookish-rotary-phone-4jq5vwwj59rqcjrpv main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
